df64d10673c24d9025f800e7bb08da11095cbefe
Zainab Oyedeji <zainab@uniswitchng.com>
2024-11-22 12:37:44 +0100

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,66 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
+const authUser = asyncHandler(async (req, res) => {
+const {email,password} = req.body;
+const user = await User.findOne({email});
+if(user){
+    generateToken(res,user._id);
+    res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email:user.email
+    })
+}
+
+
+
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
